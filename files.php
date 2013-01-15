0b689d11c5d6e45ef4ad1a89cdcd96a825b2d4a4
Milamber <milamber@gmail.com>
2013-01-15 14:02:56 -0500

    updated files.php
    added checks so it'll only return items that have a newer version

--- files.php
+++ files.php
@@ -1,9 +1,25 @@
 <?php
+if(!count($_POST)) die("Hacking Attempt...");
+$data = unserialize(stripslashes($_POST['data']));
+$types = array('skins', 'boxes', 'packages');
+
 $files = array(
 	'boxes' => array(
 		'ewpfi' => array('version' => '1.3', 'url' => 'http://voidzonemedia.com/files/ewpfi.zip'),
 		'vzm_copyright' => array('version' => '1.3', 'url' => 'http://voidzonemedia.com/files/vzm_copyright.zip')
 	)
 );
-//print_r($files);
-echo serialize($files);
+$return_data = array();
+
+foreach($types as $type) {
+	if(isset($files[$type]) && is_array($files[$type])) {
+		foreach($files[$type] as $name => $file_data){
+			if(array_key_exists($name, $data[$type]) && ($data[$type][$name] < $file_data['version']) ) {
+				$return_data[$type][$name] = $file_data;
+			}
+		}
+	}
+}
+
+if(!empty($return_data))
+	echo serialize($return_data);
